16d4a4c92ff233a63acd97afdbb16f5254eff24e
周旺 <zhouwang@phoenixtv.com>
2021-03-23 16:49:05 +0800

    add: stock 添加一些过滤规则

--- stock_read.py
+++ stock_read.py
@@ -1,6 +1,6 @@
 from bokeh.core.property.pandas import pd
 
-stockData = pd.read_csv('stock.csv', header=None)
+stockData = pd.read_csv('stock.csv', header=None,names=['类型','股票代号','股票名称','购买价格','数量','状态','成交数量','日期','交易类型','交易状态','是否竞价'])
 
 print(stockData)
 # for i in stockData:
